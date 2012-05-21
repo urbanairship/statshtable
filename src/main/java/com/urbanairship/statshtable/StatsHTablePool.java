ce7edf8c584584efd11ab8fe2406aedf5cfa0333
Dave Revell <dave@urbanairship.com>
2012-05-21 12:57:54 -0700

    License boilerplate per UA policy

--- src/main/java/com/urbanairship/statshtable/StatsHTablePool.java
+++ src/main/java/com/urbanairship/statshtable/StatsHTablePool.java
@@ -1,3 +1,7 @@
+/*
+Copyright 2012 Urban Airship and Contributors
+*/
+
 package com.urbanairship.statshtable;
 
 import org.apache.hadoop.conf.Configuration;
--- src/main/java/com/urbanairship/statshtable/UnaryFunction.java
+++ src/main/java/com/urbanairship/statshtable/UnaryFunction.java
@@ -1,3 +1,7 @@
+/*
+Copyright 2012 Urban Airship and Contributors
+*/
+
 package com.urbanairship.statshtable;
 
 public interface UnaryFunction<T> {
